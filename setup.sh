923d1897741c9e2822bbb94316dd1dface7b0de7
Fredo <luisalfredo9905@gmail.com>
2019-02-28 21:21:59 -0500

    If files already exist setup.sh overwrite it

--- setup.sh
+++ setup.sh
@@ -6,26 +6,26 @@ then
     #Ask if is root
     if [ "root" = "$(whoami)" ]
     then
-        ln -s "$(pwd)"/.tmux.conf /"$(whoami)"/.tmux.conf
-        ln -s "$(pwd)"/.vimrc /"$(whoami)"/.vimrc
-        ln -s "$(pwd)"/.bashrc /"$(whoami)"/.bashrc
-        ln -s "$(pwd)"/.profile /"$(whoami)"/.profile
+        ln -sf "$(pwd)"/.tmux.conf /"$(whoami)"/.tmux.conf
+        ln -sf "$(pwd)"/.vimrc /"$(whoami)"/.vimrc
+        ln -sf "$(pwd)"/.bashrc /"$(whoami)"/.bashrc
+        ln -sf "$(pwd)"/.profile /"$(whoami)"/.profile
 
         # Not Root
     else
-        ln -s "$(pwd)"/.tmux.conf /home/"$(whoami)"/.tmux.conf
-        ln -s "$(pwd)"/.vimrc /home/"$(whoami)"/.vimrc
-        ln -s "$(pwd)"/.bashrc /home/"$(whoami)"/.bashrc
-        ln -s "$(pwd)"/.profile /home/"$(whoami)"/.profile
+        ln -sf "$(pwd)"/.tmux.conf /home/"$(whoami)"/.tmux.conf
+        ln -sf "$(pwd)"/.vimrc /home/"$(whoami)"/.vimrc
+        ln -sf "$(pwd)"/.bashrc /home/"$(whoami)"/.bashrc
+        ln -sf "$(pwd)"/.profile /home/"$(whoami)"/.profile
     fi
 fi
 
 mkdir -p ~/.vim/autolad ~/.vim/bundle && \
     git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
-#If you are in a gnome terminal,Tilix terminal, iTerm or pathogen termina
+# ---- Preinstalations -----
+# sudo apt-get install dconf-cli
+# If you are in a gnome terminal,Tilix terminal, iTerm or pathogen terminal
+
 # https://github.com/Mayccoll/Gogh
 # bash -c  "$(wget -qO- https://git.io/vQgMr)"
-# Then pic a theme
-# Preinstalations 
-# sudo apt-get install dconf-cli
