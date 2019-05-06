6cd8f249e385c7db65b9b4fc57ae8c5f7a672692
Fredo <luisalfredo9905@gmail.com>
2019-05-06 16:03:39 -0500

    Adding spotify to polybar and misspelling in config i3 and flat orange is now no orange; is blue

---
+++ .config/polybar/spotify/previous_next.py
@@ -0,0 +1,61 @@
+#!/bin/python
+
+import sys
+import dbus
+import argparse
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument(
+    '-p',
+    '--playpause',
+    type=str,
+    metavar='Previous indicator',
+    dest='icon_previous'
+)
+
+args = parser.parse_args()
+
+
+def fix_string(string):
+    # corrects encoding for the python version used
+    if sys.version_info.major == 3:
+        return string
+    else:
+        return string.encode('utf-8')
+
+try:
+    session_bus = dbus.SessionBus()
+    spotify_bus = session_bus.get_object(
+        'org.mpris.MediaPlayer2.spotify',
+        '/org/mpris/MediaPlayer2'
+    )
+
+    spotify_properties = dbus.Interface(
+        spotify_bus,
+        'org.freedesktop.DBus.Properties'
+    )
+
+    metadata = spotify_properties.Get('org.mpris.MediaPlayer2.Player', 'Metadata')
+    status = spotify_properties.Get('org.mpris.MediaPlayer2.Player', 'PlaybackStatus')
+
+    artist = fix_string(metadata['xesam:artist'][0]) if metadata['xesam:artist'] else ''
+    song = fix_string(metadata['xesam:title']) if metadata['xesam:title'] else ''
+
+    label_with_font = '%{{T{font}}}{label}%{{T-}}'
+    icons = args.icon_previous
+    icon = label_with_font.format(font='2', label=icons)
+
+
+    if not artist and not song:
+        print('')
+    else:
+        print(icon)
+
+except Exception as e:
+    if isinstance(e, dbus.exceptions.DBusException):
+        print('')
+    else:
+        print(e)
+
+
