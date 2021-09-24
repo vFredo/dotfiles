e59dba1b98e9d3a9ece736076e4efd4ffc2aeb98
Fredo <luisalfredo9905@gmail.com>
2021-09-24 11:58:34 -0500

    separating i3 and bspwm polybars and adding missing font

--- config/polybar/launch.sh
+++ config/polybar/launch.sh
@@ -1,12 +1,29 @@
 #!/usr/bin/env bash
 
+DIR="$HOME/.config/polybar"
+
 # Terminate already running bar instances
 killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
 # If all your bars have ipc enabled, you can also use 
 # polybar-msg cmd quit
 
 # Launch main bar
-echo "---" | tee -a /tmp/polybar1.log
-polybar main >>/tmp/polybar1.log 2>&1 &
+while [ ! $# -eq 0 ]
+do
+  case "$1" in
+    --bspwm)
+      polybar -q bspwmBar -c "$DIR/config.ini" &
+      ;;
+    --i3)
+      polybar -q i3Bar -c "$DIR/config.ini" &
+      ;;
+    *) echo "Choose a valid option.";;
+  esac
+  shift
+done
 
-echo "Bar launched..."
+
