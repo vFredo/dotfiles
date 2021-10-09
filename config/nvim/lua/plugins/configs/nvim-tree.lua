c7044eb0ddee59f7edb9e3a8cd245cc6bae6646e
Fredo <luisalfredo9905@gmail.com>
2021-10-09 15:34:19 -0500

    fix: nvim-tree colors and configuration

--- config/nvim/lua/plugins/configs/nvim-tree.lua
+++ config/nvim/lua/plugins/configs/nvim-tree.lua
@@ -3,18 +3,22 @@ local tree = require("nvim-tree")
 
 g.nvim_tree_indent_markers = 1
 g.nvim_tree_git_hl = 1
-g.nvim_tree_ignore = { ".cache", ".git", "node_modules", "data" }
+g.nvim_tree_ignore = { '.cache', '.git', 'node_modules' }
 
 tree.setup{
-  update_focused_file = {
-    enable = false,
-    auto_open = false
-  },
+  disable_netrw = true,
+  hijack_netrw = true,
+  open_on_setup = false,
+  auto_close = true,
   lsp_diagnostics = true,
+  update_to_buf_dir   = {
+    enable = true,
+    auto_open = true,
+  },
   view = {
+    side = 'left',
+    auto_resize = true,
     width = 30,
     height = 30,
-    side = 'left',
-    auto_resize = false,
   }
 }
