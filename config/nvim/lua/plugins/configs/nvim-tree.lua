50fec68bd6a5ea39acb45bad6c13d712503302ce
Fredo <luisalfredo9905@gmail.com>
2021-10-09 16:48:45 -0500

    fix: nvim-tree configurations

--- config/nvim/lua/plugins/configs/nvim-tree.lua
+++ config/nvim/lua/plugins/configs/nvim-tree.lua
@@ -3,14 +3,38 @@ local tree = require("nvim-tree")
 
 g.nvim_tree_indent_markers = 1
 g.nvim_tree_git_hl = 1
-g.nvim_tree_ignore = { '.cache', '.git', 'node_modules' }
+g.nvim_tree_hide_dotfiles = 0
+g.nvim_tree_ignore = { ".git", "node_modules", ".cache" }
+g.nvim_tree_icons = {
+  default = "",
+  symlink = "",
+  git = {
+    deleted = "",
+    ignored = "◌",
+    renamed = "➜",
+    staged = "✓",
+    unmerged = "",
+    unstaged = "✗",
+    untracked = "★",
+  },
+  folder = {
+    symlink = "",
+    symlink_open = "",
+  }
+}
 
 tree.setup{
   disable_netrw = true,
   hijack_netrw = true,
   open_on_setup = false,
   auto_close = true,
-  lsp_diagnostics = true,
+  lsp_diagnostics = false,
+  hijack_cursor = true,
+  update_cwd = true,
+  update_focused_file = {
+    enable = true,
+    update_cwd = true,
+  },
   update_to_buf_dir   = {
     enable = true,
     auto_open = true,
@@ -18,7 +42,6 @@ tree.setup{
   view = {
     side = 'left',
     auto_resize = true,
-    width = 30,
-    height = 30,
+    width = 30
   }
 }
