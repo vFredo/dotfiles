66fdd7dea1f8e254125c32c7c735e37768ebd8dc
Fredo <luisalfredo9905@gmail.com>
2021-10-10 16:28:25 -0500

    fix: delete unnecessary things on gitsigns config file

--- config/nvim/lua/plugins/configs/gitsigns.lua
+++ config/nvim/lua/plugins/configs/gitsigns.lua
@@ -3,13 +3,6 @@ local gitsigns = require("gitsigns")
 gitsigns.setup{
   keymaps = { noremap = false },
   sign_priority = 5,
-  -- signs = {
-  --   add = { hl = "DiffAdd", text = "│", numhl = "GitSignsAddNr" },
-  --   change = { hl = "DiffChange", text = "│", numhl = "GitSignsChangeNr" },
-  --   changedelete = { hl = "DiffChange", text = "~", numhl = "GitSignsChangeNr" },
-  --   delete = { hl = "DiffDelete", text = "_", numhl = "GitSignsDeleteNr" },
-  --   topdelete = { hl = "DiffDelete", text = "‾", numhl = "GitSignsDeleteNr" },
-  -- },
   status_formatter = nil, -- Use default
   numhl = false,
 }
