26854e850b7635925b7cfd5be29c5856b156eb00
Fredo <luisalfredo9905@gmail.com>
2021-10-18 22:31:20 -0500

    feat: async init.lua call and formatting plugin/init.lua

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -13,9 +13,21 @@ end
 --
 require "options"
 
-require "plugins"
+local async
+async =
+  vim.loop.new_async(
+    vim.schedule_wrap(
+      function ()
+        require "plugins"
 
-require "mappings"
+        require "mappings"
 
-require "theme"
-require "highlights"
+        require "theme"
+        require "highlights"
+
+        require("impatient").enable_profile()
+        async:close()
+      end
+    )
+)
+async:send()
--- config/nvim/lua/plugins/init.lua
+++ config/nvim/lua/plugins/init.lua
@@ -1,186 +1,190 @@
 local packer = require("packer")
 local use = packer.use
 
-return packer.startup(
-  function()
-    -- Update packer manager
-    use { "wbthomason/packer.nvim", event = "VimEnter" }
+return packer.startup(function()
+  -- Update packer manager
+  use { "wbthomason/packer.nvim" }
 
-    --
-    -- Miscellaneous
-    --
+  --
+  -- Miscellaneous
+  --
 
-    -- Comment lines more easily
-    use { "tpope/vim-commentary" }
+  -- Comment lines more easily
+  use { "tpope/vim-commentary" }
 
-    -- Operation between parenthesis, brackets, etc...
-    use { "tpope/vim-surround" }
+  -- Operation between parenthesis, brackets, etc...
+  use { "tpope/vim-surround" }
 
-    -- tmux/nvim navigation
-    use { "christoomey/vim-tmux-navigator" }
+  -- tmux/nvim navigation
+  use { "christoomey/vim-tmux-navigator" }
 
-    -- Better text searches
-    use {
-      "wincent/loupe",
-      config = function()
-        vim.g.LoupeCenterResults = 0
-      end
-    }
+  -- Better text searches
+  use {
+    "wincent/loupe",
+    config = function()
+      vim.g.LoupeCenterResults = 0
+    end
+  }
 
-    --
-    -- GUI Plugins
-    --
+  -- Speed up lua modules on startup
+  use { 'lewis6991/impatient.nvim' }
 
-    -- Icons for telescope, bufferline, feline (statusline)
-    use {
-      "kyazdani42/nvim-web-devicons",
-      config = function()
-        require("nvim-web-devicons").setup{
-          override = {
-            js = {
-              name = "js",
-              icon = "",
-              color = "#FFC745"
-            }
+  --
+  -- GUI Plugins
+  --
+
+  -- Icons for telescope, bufferline, feline (statusline)
+  use {
+    "kyazdani42/nvim-web-devicons",
+    config = function()
+      require("nvim-web-devicons").setup{
+        override = {
+          js = {
+            name = "js",
+            icon = "",
+            color = "#FFC745"
           }
         }
-      end
-    }
+      }
+    end
+  }
 
-    -- Bufferline
-    use {
-      "akinsho/nvim-bufferline.lua",
-      after = "nvim-web-devicons",
-      config = function()
-        require "plugins.configs.bufferline"
-      end
-    }
+  -- Bufferline
+  use {
+    "akinsho/nvim-bufferline.lua",
+    after = "nvim-web-devicons",
+    config = function()
+      require "plugins.configs.bufferline"
+    end
+  }
 
-    -- Indentation guide
-    use {
-      "lukas-reineke/indent-blankline.nvim",
-      config = function()
-        require "plugins.configs.blankline"
-      end
-    }
+  -- Indentation guide
+  use {
+    "lukas-reineke/indent-blankline.nvim",
+    config = function()
+      require "plugins.configs.blankline"
+    end
+  }
 
-    -- Statusline
-    use {
-      'famiu/feline.nvim',
-      branch = 'master',
-      after = "nvim-web-devicons",
-      config = function()
-        require "plugins.configs.statusline"
-      end
-    }
+  -- Statusline
+  use {
+    'famiu/feline.nvim',
+    branch = 'master',
+    after = "nvim-web-devicons",
+    config = function()
+      require "plugins.configs.statusline"
+    end
+  }
 
-    -- Tree view of the files
-    use {
-      "kyazdani42/nvim-tree.lua",
-      config = function()
-        require "plugins.configs.nvim-tree"
-      end
-    }
+  -- Tree view of the files
+  use {
+    "kyazdani42/nvim-tree.lua",
+    config = function()
+      require "plugins.configs.nvim-tree"
+    end
+  }
 
-    -- Theme
-    use {
-      "norcalli/nvim-base16.lua",
-      requires = { "norcalli/nvim.lua" }
-    }
+  -- Theme
+  use {
+    "norcalli/nvim-base16.lua",
+    requires = { "norcalli/nvim.lua" }
+  }
 
-    -- Smooth scrolling
-    use {
-      "karb94/neoscroll.nvim",
-      config = function()
-        require("neoscroll").setup()
-      end
-    }
+  -- Smooth scrolling
+  use {
+    "karb94/neoscroll.nvim",
+    config = function()
+      require("neoscroll").setup()
+    end
+  }
 
-    -- Fuzzy finder
-    use {
-      "nvim-telescope/telescope.nvim",
-      requires = {
-        "nvim-lua/plenary.nvim",
-        { "nvim-telescope/telescope-fzf-native.nvim", run = "make" },
-      },
-      config = function()
-        require "plugins.configs.telescope"
-      end
-    }
+  -- Fuzzy finder
+  use {
+    "nvim-telescope/telescope.nvim",
+    requires = {
+      "nvim-lua/plenary.nvim",
+      "nvim-lua/popup.nvim",
+      { "nvim-telescope/telescope-fzf-native.nvim", run = "make" },
+    },
+    config = function()
+      require "plugins.configs.telescope"
+    end
+  }
 
-    --
-    -- Git
-    --
-    use {
-      "lewis6991/gitsigns.nvim",
-      config = function()
-        require "plugins.configs.gitsigns"
-      end
-    }
+  --
+  -- Git
+  --
+  use {
+    "lewis6991/gitsigns.nvim",
+    config = function()
+      require "plugins.configs.gitsigns"
+    end
+  }
 
-    use { "tpope/vim-fugitive" }
+  use { "tpope/vim-fugitive" }
 
-    --
-    -- LSP
-    --
-    use {
-      "neovim/nvim-lspconfig",
-      config = function()
-        require "plugins.configs.lspconfig"
-      end
-    }
+  --
+  -- LSP
+  --
+  use {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require "plugins.configs.lspconfig"
+    end
+  }
 
-    -- Autocomplete
-    use {
-      'ms-jpq/coq_nvim',
-      branch = 'coq',
-      setup = function()
-        vim.g.coq_settings = {
-          auto_start = 'shut-up',
-          ["keymap.recommended"] = false,
-          ["keymap.jump_to_mark"] = "<c-b>",
-          ["keymap.manual_complete"] = "<c-n>",
-          clients = { tabnine = { enabled = true } }
-        }
-      end,
-      config =  function()
-        require "plugins.configs.coq"
-      end
-    }
+  -- Autocomplete
+  use {
+    'ms-jpq/coq_nvim',
+    branch = 'coq',
+    setup = function()
+      vim.g.coq_settings = {
+        auto_start = 'shut-up',
+        ["keymap.recommended"] = false,
+        ["keymap.jump_to_mark"] = "<c-b>",
+        ["keymap.manual_complete"] = "<c-n>",
+        clients = { tabnine = { enabled = true } }
+      }
+    end,
+    config =  function()
+      require "plugins.configs.coq"
+    end
+  }
 
-    -- Snippets and thirdparty support for coq_nvim
-    use { 'ms-jpq/coq.artifacts', branch = 'artifacts' }
-    use { 'ms-jpq/coq.thirdparty', branch = '3p'}
+  -- Snippets and thirdparty support for coq_nvim
+  use { 'ms-jpq/coq.artifacts', branch = 'artifacts' }
+  use { 'ms-jpq/coq.thirdparty', branch = '3p'}
 
-    --
-    -- Web development Javascript/React/Typescript
-    --
-    use {
-      "nvim-treesitter/nvim-treesitter",
-      requires = {
-        "JoosepAlviste/nvim-ts-context-commentstring",
-        "windwp/nvim-ts-autotag",
-        "windwp/nvim-autopairs"
-      },
-      branch = "0.5-compat",
-      run = ":TSUpdate",
-      config = function()
-        require "plugins.configs.treesitter"
-      end
-    }
+  --
+  -- Web development Javascript/React/Typescript
+  --
+  use {
+    "nvim-treesitter/nvim-treesitter",
+    requires = {
+      "JoosepAlviste/nvim-ts-context-commentstring",
+      "windwp/nvim-ts-autotag",
+      "windwp/nvim-autopairs"
+    },
+    branch = "0.5-compat",
+    run = ":TSUpdate",
+    config = function()
+      require "plugins.configs.treesitter"
+    end
+  }
 
-    -- Color highlighter for hex, rgb, etc...
-    use {
-      "norcalli/nvim-colorizer.lua",
-      config = function()
-        require("colorizer").setup({"*"}, {
-          css = { rgb_fn = true },
-          RGB = true, -- #RGB hex codes
-          RRGGBB = true, -- #RRGGBB hex codes
-          RRGGBBAA = true, -- #RRGGBBAA hex codes
-          names = false, -- "Name" codes like 'blue'
-        })
-      end
-    }
-  end
-)
+  -- Color highlighter for hex, rgb, etc...
+  use {
+    "norcalli/nvim-colorizer.lua",
+    event = "BufRead",
+    config = function()
+      require("colorizer").setup({"*"}, {
+        css = { rgb_fn = true },
+        RRGGBB = true, -- #RRGGBB hex codes
+        RRGGBBAA = true, -- #RRGGBBAA hex codes
+        names = false, -- "Name" codes like 'blue'
+        mode = "background"
+      })
+      vim.cmd "ColorizerReloadAllBuffers"
+    end
+  }
+end)
