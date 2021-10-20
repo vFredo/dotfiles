174d34ac81a106840c85fb38d0c1f01c69bb8801
Fredo <luisalfredo9905@gmail.com>
2021-10-20 18:07:50 -0500

    fix: coq and npairs integration and add npair rule

--- config/nvim/lua/plugins/configs/coq.lua
+++ config/nvim/lua/plugins/configs/coq.lua
@@ -1,28 +1,26 @@
 -- For coq bidnings
 local remap = vim.api.nvim_set_keymap
 
-remap('i', '<esc>', [[pumvisible() ? "<c-e><esc>" : "<esc>"]], { expr = true, noremap = true })
-remap('i', '<c-c>', [[pumvisible() ? "<c-e><c-c>" : "<c-c>"]], { expr = true, noremap = true })
-remap('i', '<tab>', [[pumvisible() ? (complete_info().selected == -1 ? "\<C-e><CR>" : "\<C-y>") : "\<Tab>"]], { expr = true, noremap = true })
+remap('i', '<Esc>', [[pumvisible() ? "<C-e><Esc>" : "<Esc>"]], { expr = true, noremap = true })
+remap('i', '<C-c>', [[pumvisible() ? "<C-e><C-c>" : "<C-c>"]], { expr = true, noremap = true })
+remap('i', '<Tab>', [[pumvisible() ? (complete_info().selected == -1 ? "\<C-e><CR>" : "\<C-y>") : "\<Tab>"]], { expr = true, noremap = true })
 
-local npairs = require('nvim-autopairs')
-npairs.setup({ map_bs = false })
+local npairs = require("nvim-autopairs")
 
 _G.MUtils= {}
 
 MUtils.CR = function()
   if vim.fn.pumvisible() ~= 0 then
     if vim.fn.complete_info({ 'selected' }).selected ~= -1 then
-      return npairs.esc('<c-y>')
+      return npairs.esc('<C-y>')
     else
-      return npairs.esc('<c-e>') .. npairs.autopairs_cr()
+      return npairs.esc('<C-e>') .. npairs.autopairs_cr()
     end
   else
     return npairs.autopairs_cr()
   end
 end
-
-remap('i', '<cr>', 'v:lua.MUtils.CR()', { expr = true, noremap = true })
+remap('i', '<CR>', 'v:lua.MUtils.CR()', { expr = true, noremap = true })
 
 MUtils.BS = function()
   if vim.fn.pumvisible() ~= 0 and vim.fn.complete_info({ 'mode' }).mode == 'eval' then
@@ -31,4 +29,4 @@ MUtils.BS = function()
     return npairs.autopairs_bs()
   end
 end
-remap('i', '<bs>', 'v:lua.MUtils.BS()', { expr = true, noremap = true })
+remap('i', '<BS>', 'v:lua.MUtils.BS()', { expr = true, noremap = true })
--- config/nvim/lua/plugins/configs/treesitter.lua
+++ config/nvim/lua/plugins/configs/treesitter.lua
@@ -1,9 +1,37 @@
 local ts_config = require("nvim-treesitter.configs")
 local npairs = require("nvim-autopairs")
+local Rule = require("nvim-autopairs.rule")
 
 npairs.setup{
   check_ts = true,
-  enable_check_bracket_line = false,
+  map_bs = false
+}
+
+-- Add spaces between parentheses
+npairs.add_rules {
+  Rule(' ', ' ')
+    :with_pair(function (opts)
+      local pair = opts.line:sub(opts.col - 1, opts.col)
+      return vim.tbl_contains({ '()', '[]', '{}' }, pair)
+    end),
+  Rule('( ', ' )')
+    :with_pair(function() return false end)
+    :with_move(function(opts)
+      return opts.prev_char:match('.%)') ~= nil
+    end)
+    :use_key(')'),
+  Rule('{ ', ' }')
+    :with_pair(function() return false end)
+    :with_move(function(opts)
+      return opts.prev_char:match('.%}') ~= nil
+    end)
+    :use_key('}'),
+  Rule('[ ', ' ]')
+    :with_pair(function() return false end)
+    :with_move(function(opts)
+      return opts.prev_char:match('.%]') ~= nil
+    end)
+    :use_key(']')
 }
 
 ts_config.setup{
