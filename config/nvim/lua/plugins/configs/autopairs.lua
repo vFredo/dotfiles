f5f164ef209a5f8861556aa69297fa5ab44d38e3
Fredo <luisalfredo9905@gmail.com>
2021-10-28 11:00:15 -0500

    fix: update changes needed after updating plugins cmp, autopairs and telescope

--- config/nvim/lua/plugins/configs/autopairs.lua
+++ config/nvim/lua/plugins/configs/autopairs.lua
@@ -41,3 +41,8 @@ npairs.add_rules {
     return opts.prev_char:match ".%]" ~= nil
   end):use_key "]",
 }
+
+-- cmp integration
+local cmp_autopairs = require('nvim-autopairs.completion.cmp')
+local cmp = require('cmp')
+cmp.event:on( 'confirm_done', cmp_autopairs.on_confirm_done())
