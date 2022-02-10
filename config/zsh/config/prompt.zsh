b185f0c792c11559e4978d538623f83011eb5840
Fredo <luisalfredo9905@gmail.com>
2022-02-09 20:01:22 -0500

    fix: zsh prompt colors, fix alias for zsh config and deleting backups

--- config/zsh/config/prompt.zsh
+++ config/zsh/config/prompt.zsh
@@ -19,6 +19,10 @@ prompt pure
 # Prompt styles.
 zstyle :prompt:pure:prompt:error color yellow
 zstyle :prompt:pure:prompt:success color red
+zstyle :prompt:pure:git:branch color yellow
+zstyle :prompt:pure:git:arrow color magenta
+zstyle :prompt:pure:git:dirty color green
+zstyle :prompt:pure:execution_time color cyan
 
 # This prompt pops when zsh think that your last command is misspelled
 export SPROMPT="Correct %F{red}'%R'%f to %F{orange}'%r'%f [%B%Uy%u%bes, %B%Un%u%bo, %B%Ue%u%bdit, %B%Ua%u%bbort]? "
