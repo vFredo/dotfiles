b54cf7c1e7defb039818b2f731042a51bee3f3e1
Fredo <luisalfredo9905@gmail.com>
2022-04-18 11:49:18 -0500

    fix: cmp keybindings on the command line

--- config/nvim/lua/plugins/configs/cmp-completion.lua
+++ config/nvim/lua/plugins/configs/cmp-completion.lua
@@ -88,14 +88,12 @@ cmp.setup {
 
 -- Use buffer source for `/` (if you enabled `native_menu`, this won't work anymore).
 cmp.setup.cmdline('/', {
+  mapping = cmp.mapping.preset.cmdline(),
   sources = { { name = 'buffer' } }
 })
 
 -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
 cmp.setup.cmdline(':', {
-  sources = cmp.config.sources({
-    { name = 'path' }
-  }, {
-    { name = 'cmdline' }
-  })
+  mapping = cmp.mapping.preset.cmdline(),
+  sources = { { name = 'cmdline' } }
 })
