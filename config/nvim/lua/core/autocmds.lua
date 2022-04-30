a2d0629e3d8b52763875b9eb92f296ae8a3ff88f
Fredo <luisalfredo9905@gmail.com>
2022-04-29 20:17:25 -0500

    feat: formatting autocmds

--- config/nvim/lua/core/autocmds.lua
+++ config/nvim/lua/core/autocmds.lua
@@ -1,8 +1,6 @@
 -- Helpers
 local autocmd = vim.api.nvim_create_autocmd
-local augroup = vim.api.nvim_create_augroup
-
--- Autocommand groups
+local augroup = vim.api.nvim_create_augroup -- Autocommand groups
 local gen_group = augroup("general_options", { clear = true })
 local ft_group = augroup("filetype_specific", { clear = true })
 local yank_group = augroup("highlight_yank", { clear = true })
@@ -24,16 +22,14 @@ autocmd({ "FileType" },
   { group = ft_group, pattern = "*", command = "set fo-=c fo-=r fo-=o fo+=j fo+=n" })
 
 -- Check spell on git commits
-autocmd({ "FileType" },
-  {
+autocmd({ "FileType" }, {
   group = ft_group,
   pattern = "gitcommit",
   command = "lua require('core.utils').toggleSpelling('ft')"
 })
 
 -- highlight yank text
-autocmd({ "TextYankPost" },
-  {
+autocmd({ "TextYankPost" }, {
   group = yank_group,
   pattern = "*",
   command = [[silent! lua vim.highlight.on_yank() { higroup="IncSearch", timeout=500 }]]
