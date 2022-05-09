1ffbe3a45cf9eec0ebf4653dd6ee337b0d86a5e2
Fredo <luisalfredo9905@gmail.com>
2022-05-09 17:01:02 -0500

    fix: autocmds for highlights yank text and restore cursor pos when exit

--- config/nvim/lua/core/autocmds.lua
+++ config/nvim/lua/core/autocmds.lua
@@ -5,34 +5,39 @@ local augroup = vim.api.nvim_create_augroup
 -- Autocommand groups
 local gen_group = augroup("general_options", { clear = true })
 local ft_group = augroup("filetype_specific", { clear = true })
-local yank_group = augroup("highlight_yank", { clear = true })
 
 -- Responsive panel/window size
 autocmd({ "VimResized" },
   { group = gen_group, pattern = "*", command = "wincmd =" })
 
--- Restore cursor position
-autocmd({ "BufRead" },
-  { group = gen_group, pattern = "*", command = [[call setpos(".", getpos("'\""))]] })
+-- Open a file from its last left off position
+autocmd("BufReadPost", {
+  callback = function()
+    if not vim.fn.expand("%:p"):match ".git" and vim.fn.line "'\"" > 1 and vim.fn.line "'\"" <= vim.fn.line "$" then
+      vim.cmd "normal! g'\""
+      vim.cmd "normal zz"
+    end
+  end,
+})
 
 -- Check if file changed when its window is focus, better than 'autoread'
-autocmd({ "FocusGained" },
+autocmd("FocusGained",
   { group = gen_group, pattern = "*", command = "checktime" })
 
 -- Format options
-autocmd({ "FileType" },
+autocmd("FileType",
   { group = ft_group, pattern = "*", command = "set fo-=c fo-=r fo-=o fo+=j fo+=n" })
 
 -- Check spell on git commits
-autocmd({ "FileType" }, {
+autocmd("FileType", {
   group = ft_group,
   pattern = "gitcommit",
   command = "lua require('core.utils').toggleSpelling('ft')"
 })
 
 -- Highlight yank text
-autocmd({ "TextYankPost" }, {
-  group = yank_group,
-  pattern = "*",
-  command = [[silent! lua vim.highlight.on_yank() { higroup="IncSearch", timeout=500 }]]
+autocmd("TextYankPost", {
+  callback = function()
+    vim.highlight.on_yank { higroup = "IncSearch", timeout = 200 }
+  end,
 })
