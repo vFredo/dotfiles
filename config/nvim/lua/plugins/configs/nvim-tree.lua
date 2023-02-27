b6151d097badae56137a4df85c07c7c8b8e35639
Fredo <luisalfredo9905@gmail.com>
2023-02-27 08:35:35 -0500

    fix: update deprecated way of mapping on nvim-tree

--- config/nvim/lua/plugins/configs/nvim-tree.lua
+++ config/nvim/lua/plugins/configs/nvim-tree.lua
@@ -3,43 +3,51 @@ if not ok then
   vim.notify("Couldn't load nvim-tree " .. tree .. "\n")
 end
 
+-- Mappings
+local on_attach = function(bufnr)
+  local api = require('nvim-tree.api')
+  local map = vim.keymap.set
+  local opts = function(desc)
+    return { desc = 'nvim-tree: ' .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+  end
+  map('n', 'L', api.tree.change_root_to_node, opts('CD'))
+  map('n', 'H', api.tree.change_root_to_parent, opts('Up'))
+  map('n', '<C-k>', api.node.show_info_popup, opts('Info'))
+  map('n', '<C-t>', api.node.open.tab, opts('Open: New Tab'))
+  map('n', '<C-v>', api.node.open.vertical, opts('Open: Vertical Split'))
+  map('n', '<C-s>', api.node.open.horizontal, opts('Open: Horizontal Split'))
+  map('n', '<Tab>', api.node.open.preview, opts('Open Preview'))
+  map('n', '.', api.node.run.cmd, opts('Run Command'))
+  map('n', 'g?', api.tree.toggle_help, opts('Help'))
+  map('n', 'o', api.node.open.edit, opts('Open'))
+  map('n', 'O', api.node.open.no_window_picker, opts('Open: No Window Picker'))
+  map('n', 'P', api.node.navigate.parent, opts('Parent Directory'))
+  map('n', 'q', api.tree.close, opts('Close'))
+  map('n', 'r', api.fs.rename, opts('Rename'))
+  map('n', 'R', api.tree.reload, opts('Refresh'))
+  map('n', 'c', api.fs.create, opts('Create'))
+  map('n', 'D', api.fs.remove, opts('Delete'))
+  map('n', 'd', api.fs.cut, opts('Cut'))
+  map('n', 'p', api.fs.paste, opts('Paste'))
+  map('n', 'gy', api.fs.copy.absolute_path, opts('Copy Absolute Path'))
+  map('n', 'y', api.fs.copy.filename, opts('Copy Name'))
+  map('n', 'Y', api.fs.copy.node, opts('Copy'))
+  map('n', '<2-LeftMouse>', api.node.open.edit, opts('Open'))
+  map('n', '<CR>', api.node.open.edit, opts('Open'))
+  map('n', 'l', api.node.open.edit, opts('Open'))
+end
+
 tree.setup {
   disable_netrw = true,
   hijack_netrw = true,
-  open_on_setup = false,
   hijack_cursor = true,
   update_cwd = true,
+  on_attach = on_attach,
   update_focused_file = { enable = true, update_cwd = false },
   git = { ignore = false },
   view = {
     width = 32,
     side = 'left',
-    preserve_window_proportions = true,
-    mappings = {
-      custom_only = true,
-      list = {
-        { key = { 'l', '<CR>', '<2-LeftMouse>' }, action = 'edit' },
-        { key = 'L',                              action = 'cd' },
-        { key = '<C-s>',                          action = 'split' },
-        { key = '<C-v>',                          action = 'vsplit' },
-        { key = '<C-t>',                          action = 'tabnew' },
-        { key = 'h',                              action = 'close_node' },
-        { key = 'i',                              action = 'preview' },
-        { key = 'R',                              action = 'refresh' },
-        { key = 'c',                              action = 'create' },
-        { key = 'D',                              action = 'remove' },
-        { key = 'r',                              action = 'rename' },
-        { key = 'd',                              action = 'cut' },
-        { key = 'y',                              action = 'copy' },
-        { key = 'p',                              action = 'paste' },
-        { key = 'gyn',                            action = 'copy_name' },
-        { key = 'gyp',                            action = 'copy_path' },
-        { key = 'gya',                            action = 'copy_absolute_path' },
-        { key = 'H',                              action = 'dir_up' },
-        { key = 's',                              action = 'system_open' },
-        { key = 'q',                              action = 'close' },
-      },
-    },
   },
   filters = {
     dotfiles = false,
@@ -58,11 +66,11 @@ tree.setup {
         default = "",
         symlink = "",
         git = {
-          deleted = "",
+          deleted = "契",
           ignored = "◌",
           renamed = "➜",
           unmerged = "",
-          staged = "S",
+          staged = "+",
           unstaged = "",
           untracked = "U"
         },
