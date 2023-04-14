e6b1564f189d478d1b545d667dc96beda6ee197d
Fredo <luisalfredo9905@gmail.com>
2023-04-14 16:10:30 -0500

    fix(zsh): aliases, file names and general fixes

--- config/zsh/config/settings.zsh
+++ config/zsh/config/settings.zsh
@@ -66,7 +66,7 @@ function -auto-ls-after-cd() {
     emulate -L zsh
     # Only in response to a user-initiated `cd`, not indirectly (eg. via another function).
     if [ "$ZSH_EVAL_CONTEXT" = "toplevel:shfunc" ]; then
-      ls --almost-all --classify --color=always --group-directories-first --literal --si
+      ls --almost-all --classify --color=always --group-directories-first --literal
     fi
 }
 add-zsh-hook chpwd -auto-ls-after-cd
