2a85394032b5d7ef6fdda24c1c21e720caad926d
Fredo <luisalfredo9905@gmail.com>
2023-11-01 22:42:00 -0500

    feat(nvim): update snippet engine and tmux style

--- config/nvim/lua/plugins/completion.lua
+++ config/nvim/lua/plugins/completion.lua
@@ -35,7 +35,7 @@ return {
       "hrsh7th/cmp-path",
       "hrsh7th/cmp-buffer",
       "hrsh7th/cmp-cmdline",
-      { "L3MON4D3/LuaSnip", version = "1.*" }, -- snippets engine
+      { "L3MON4D3/LuaSnip", version = "v2.*", build = "make install_jsregexp" }, -- snippets engine
       "saadparwaiz1/cmp_luasnip",              -- integrations
     },
     config = function()
