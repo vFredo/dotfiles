bc79519dc18354e609e1f96548c66fe14f3f8bbf
Fredo <luisalfredo9905@gmail.com>
2024-02-09 12:04:07 -0500

    fix: zoom notifications

--- config/qtile/modules/layouts.py
+++ config/qtile/modules/layouts.py
@@ -36,6 +36,7 @@ floating_layout = layout.Floating(
         Match(wm_class='makebranch'),  # gitk
         Match(wm_class='maketag'),  # gitk
         Match(wm_class='ssh-askpass'),  # ssh-askpass
+        Match(title='zoom'), # zoom notifications
         Match(title='branchdialog'),  # gitk
         Match(title='pinentry'),  # GPG key password entry
         Match(title='Volume Control'),  # GPG key password entry
